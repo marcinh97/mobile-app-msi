1473ccf4012abe632f2f1314a32c0ecf737b0bbe
marcinh97 <marcin.horak@gmail.com>
2020-04-07 21:35:14 +0200

    FCW: Removed unnecessary prompts

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -22,17 +22,6 @@ export const Home = (props: IHomeProp) => {
         ) : (
           <div>
             <Alert color="warning">
-              If you want to
-              <Link to="/login" className="alert-link">
-                {' '}
-                sign in
-              </Link>
-              , you can try the default accounts:
-              <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
-              <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
-            </Alert>
-
-            <Alert color="warning">
               You do not have an account yet?&nbsp;
               <Link to="/account/register" className="alert-link">
                 Register a new account
